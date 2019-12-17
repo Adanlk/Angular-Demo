8d49ceb3607ca51a7dd120e0d7d1e6e0cceab738
Adanlk <455224099@qq.com>
2019-12-17 23:43:16 +0800

    惰性加载

--- src/app/orders/orders-routing.module.ts
+++ src/app/orders/orders-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OrderListComponent } from './order-list/order-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: OrderListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
